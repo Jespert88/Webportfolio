715d74997274dd3a95a840c8ccb414e7c256e626
Jesper <jesper_truedsson@hotmail.com>
2020-11-25 13:10:56 +0100

    first commit

---
+++ portfolio-app/src/components/appSideBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="Sidebar">
+    <div class="centerContainer, padding-lg">
+      <img src="../assets/selfie.jpg" class="img-fluid" id="Selfie" />
+    </div>
+
+    <div class="centerContainer">
+      <h1 class="text-center, title-sm-white">Jesper Truedsson</h1>
+      <h1 class="text-center, text-sm-white">Junior webbutvecklare</h1>
+    </div>
+
+    <div class="centerContainer">
+      <div class="flex">
+        <div class="padding-lg">
+          <a href="https://www.linkedin.com/in/jesper-truedsson-812806142/">
+            <img src="../assets/linkedin.png" class="img-fluid, linkIcons" />
+            <p class="text-sm-white">LinkedIn</p>
+          </a>
+        </div>
+        <div class="padding-lg">
+          <a href="mailto:jespertruedsson88@gmail.com">
+            <img src="../assets/gmail.png" class="img-fluid, linkIcons" />
+            <p class="text-sm-white">Gmail</p>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="centerContainer, padding-sm">
+      <div class="flex">
+        <img src="../assets/smartphone.png" class="img-fluid" id="phoneIcon" />
+        <p class="text-sm-white"><b>Mobil:</b> 0768970571</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+@import "../assets/css/appSideBar.css";
+@import "../assets/css/globalStyles.css";
+</style>
---
+++ portfolio-app/src/views/Index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="Index, indexFlex">
+    <!-- Sidebar -->
+    <div class="leftContainer">
+      <appSideBar />
+    </div>
+
+    <!-- Main Content -->
+    <div class="rightContainer">
+      <div class="margin-xl">
+        <div class="centerContainer">
+          <h1 class="title-lg">Tjena Jesper här!</h1>
+        </div>
+      </div>
+
+      <div class="padding-lg">
+        <p class="text-sm">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+
+      <div class="centerContainer">
+        <h1 class="title-lg">Heroku projekt</h1>
+      </div>
+
+      <div class="centerContainer">
+        <div class="padding-lg">
+          <div class="grid-col-3">
+            <a href="https://naturkollen.herokuapp.com/" class="projectLinks">
+              <img
+                src="../assets/naturkollen.png"
+                class="img-fluid, projectImg"
+              />
+              <p class="text-sm">Naturkollen</p></a
+            >
+            <a
+              href="https://react-template-oak.herokuapp.com/"
+              class="projectLinks"
+            >
+              <img src="../assets/movies.png" class="img-fluid, projectImg" />
+              <p class="text-sm">React movie app</p></a
+            >
+            <a
+              href="https://gearfornature.herokuapp.com/#/"
+              class="projectLinks"
+            >
+              <img
+                src="../assets/gearForNature.png"
+                class="img-fluid, projectImg"
+              />
+              <p class="text-sm">gearForNature</p></a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import appSideBar from "@/components/appSideBar.vue";
+
+export default {
+  name: "Index",
+  components: {
+    appSideBar,
+  },
+};
+</script>
+<style>
+@import "../assets/css/Index.css";
+@import "../assets/css/globalStyles.css";
+</style>
