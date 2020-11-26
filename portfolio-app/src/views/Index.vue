af23a59a1c4c048d186e140d89c61339ead8a5de
Jesper <jesper_truedsson@hotmail.com>
2020-11-26 10:34:11 +0100

    fixed intro text

--- portfolio-app/src/views/Index.vue
+++ portfolio-app/src/views/Index.vue
@@ -13,8 +13,15 @@
         </div>
       </div>
 
-      <div class="padding-lg">
-        <p class="text-sm">Vad skulle jag kunna skriva här?</p>
+      <div class="centerContainer">
+        <div class="padding-lg">
+          <p class="text-sm">
+            Är en kille som brinner för natur och teknik. Är ständigt
+            instresserad av lära mig nya saker inom programmering och teknik.<br />
+            Dessa webbappar är hobbyprojekt jag har byggt för att lära mig
+            ramverken och för att visa vad jag kan göra just nu.
+          </p>
+        </div>
       </div>
 
       <div class="centerContainer">
